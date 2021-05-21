553d2744cdc8bb7cf66db5d257de531a6d14a9a4
Seungup Noh <su.noh@kakao.com>
2021-05-21 23:50:30 +0900

    update 기본세팅

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 
+let APP_PORT = 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  init();
+  console.log(APP_PORT);
+  await app.listen(APP_PORT);
 }
 bootstrap();
+
+function init() {
+  console.log(process.env.STAGES);
+  if (!process.env.STAGES) {
+    process.env.STAGES = "LOCAL";
+    APP_PORT = 3001;
+  }
+}
