6374aa09e2b48ef78dd0af07c9812efd096ad6fb
Seungup Noh <su.noh@kakao.com>
2021-05-21 23:55:44 +0900

    update 불필요한 콘솔로그 삭제

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,11 @@ let APP_PORT = 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   init();
-  console.log(APP_PORT);
   await app.listen(APP_PORT);
 }
 bootstrap();
 
 function init() {
-  console.log(process.env.STAGES);
   if (!process.env.STAGES) {
     process.env.STAGES = "LOCAL";
     APP_PORT = 3001;
