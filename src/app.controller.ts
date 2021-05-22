2f61052a6b959d6c45197a3bc41d24f3a47f2e36
Seungup Noh <su.noh@kakao.com>
2021-05-22 15:26:30 +0900

    update 더미 데이터 추가

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from "@nestjs/common";
-import { AppService, HelloRes } from "./app.service";
-
+import { Controller, Get, HttpStatus, Param, Res } from "@nestjs/common";
+import { Response } from "express";
+import { AppService } from "./app.service";
+import { version } from "../package.json";
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): HelloRes {
-    return this.appService.getHello();
+  @Get("ping")
+  sendStatusOK(@Res() res: Response): void {
+    res.sendStatus(HttpStatus.OK);
+  }
+
+  @Get("version")
+  getVersion() {
+    return version;
+  }
+
+  @Get(":userId/glass")
+  getGlasses(@Param() param) {
+    return this.appService.getGlasses(param.userId);
+  }
+
+  @Get(":userId/today-todos")
+  getTodayTodos(@Param() param) {
+    return this.appService.getTodayTodos(param.userId);
   }
 }
