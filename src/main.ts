189800ad391f657e07c6d277c7ef76e0edbc548f
Seungup Noh <su.noh@kakao.com>
2021-05-23 09:56:33 +0900

    update | add APP_PORT 선언부 변경 및 환경변수 자동주입 위치 변경

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-
-let APP_PORT = 3000;
+import { APP_PORT } from "./common/common.define";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  init();
   await app.listen(APP_PORT);
 }
 bootstrap();
-
-function init() {
-  if (!process.env.STAGES) {
-    process.env.STAGES = "LOCAL";
-    APP_PORT = 3001;
-  }
-}
