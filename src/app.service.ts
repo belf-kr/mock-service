ced49f037d0858533065cb79d45a5fe67c0a7520
Kyungeun <ruddms936@naver.com>
2021-05-28 21:27:14 +0900

    add: 테스트 코드 추가

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,6 +4,10 @@ import { version } from "../package.json";
 
 @Injectable()
 export class AppService {
+  getHello(): string {
+    return "Hello World!";
+  }
+
   getVersion(): string {
     return version;
   }
