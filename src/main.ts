24ac3c8259886e168b0164695afb7ff6075406b3
Kyungeun <ruddms936@naver.com>
2021-05-28 19:10:50 +0900

    fix: runtime

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from "@nestjs/core";
+
 import { AppModule } from "./app.module";
-import { APP_PORT } from "./common/common.define";
+
+import { version } from "../package.json";
+
+console.log(`version: ${version}`);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(APP_PORT);
+  await app.listen(3000);
 }
 bootstrap();
