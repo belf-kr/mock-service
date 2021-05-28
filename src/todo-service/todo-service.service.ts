cd020fc9fd55de5e052a3f8367759307824ca636
Kyungeun <ruddms936@naver.com>
2021-05-28 22:16:07 +0900

    up: 명확한 키워드로 변경

--- src/todo-service/todo-service.service.ts
+++ src/todo-service/todo-service.service.ts
@@ -14,22 +14,22 @@ function randomInt0to10() {
   return Math.floor(Math.random() * 10);
 }
 
-type IGlass = {
+type Glass = {
   date: number;
   count: number;
 };
-type IGlasses = {
-  glass_list: IGlass[];
+type Glasses = {
+  glass_list: Glass[];
 };
 
-type ITodo = {
+type Todo = {
   reminder_id: string;
   reminder_title: string;
   course_title: string;
   color: string;
 };
-type ITodos = {
-  todo_list: ITodo[];
+type Todos = {
+  todo_list: Todo[];
 };
 
 interface IServiceEndpoint {
@@ -37,24 +37,24 @@ interface IServiceEndpoint {
    * 요약정보 가져오기(잔디)
    * @param userId
    */
-  getPlantingGlass(userId: string): Promise<IGlasses>;
+  getPlantingGlass(userId: string): Promise<Glasses>;
   /**
    * 오늘 할 일 가져오기
    * @param userId
    */
-  getTodayTodos(userId: string): Promise<ITodos>;
+  getTodayTodos(userId: string): Promise<Todos>;
 }
 
 @Injectable()
 export class TodoServiceService implements IServiceEndpoint {
-  async getPlantingGlass(userId: string): Promise<IGlasses> {
-    console.log(`getGlasses : ${userId}`);
-    const result: IGlass[] = [];
+  async getPlantingGlass(userId: string): Promise<Glasses> {
+    console.log(`getPlantingGlass: ${userId}`);
+    const result: Glass[] = [];
     const now = new Date();
     const size = 23 * 7 + (new Date().getDay() + 1);
     for (let index = 0; index < size; index++) {
       const date = new Date();
-      const g: IGlass = {
+      const g: Glass = {
         date: new Date(date.setDate(now.getDate() - index)).getTime(),
         count: randomInt0to10(),
       };
@@ -65,8 +65,8 @@ export class TodoServiceService implements IServiceEndpoint {
     };
   }
 
-  async getTodayTodos(userId: string): Promise<ITodos> {
-    console.log(`getTodayTodos : ${userId}`);
+  async getTodayTodos(userId: string): Promise<Todos> {
+    console.log(`getTodayTodos: ${userId}`);
     return {
       todo_list: [
         {
