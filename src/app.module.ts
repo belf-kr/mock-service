f49856f1719f1b074ef60c1bf867b8fed9b817d1
JP3Pe <45834750+JP3Pe@users.noreply.github.com>
2021-06-11 19:42:49 +0900

    서버의 listen port를 동적으로 주입받도록 설정 및 k8s 스케줄러 개선 (#13)
    * add: Npm(NodeJS) 설정 주입을 todo와 동일하게 수정
    
    closes #11
    
    * close: 팟 스케줄러 개선 (#12)
    
    * docs: 환경 변수 목록 추가
    
    * up: 워크폴로우 이름 변경
    
    Co-authored-by: 박경은 <ruddms936@naver.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,26 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from "joi";
 
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-
 import { TodoServiceModule } from "./todo-service/todo-service.module";
+import AppConfig from "./config/app.config";
 
 @Module({
-  imports: [TodoServiceModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: false,
+      load: [AppConfig],
+      // prod 환경의 환경변수는 모두 k8s가 컨트롤
+      ignoreEnvFile: process.env.NODE_ENV === "production",
+      envFilePath: process.env.NODE_ENV === "development" ? ".env.dev" : "",
+      validationSchema: Joi.object({
+        SERVER_PORT: Joi.number().default(3000),
+      }),
+    }),
+    TodoServiceModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
