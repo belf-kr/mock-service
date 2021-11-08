ef74c186c7691a7ad9caca559dc539b9f097f5bd
JP3Pe <45834750+JP3Pe@users.noreply.github.com>
2021-11-08 12:06:46 +0900

    사용되지 않는 API 관련 코드 삭제 (#24)
    * del: 미사용 코드 삭제
    
    * up: 버전 정보 업데이트

--- src/todo-service/course-tag/course-tag.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from "@nestjs/common";
-
-@Controller("course-tag")
-export class CourseTagController {}
--- src/todo-service/todo-service.module.ts
+++ src/todo-service/todo-service.module.ts
@@ -2,15 +2,9 @@ import { Module } from "@nestjs/common";
 
 import { TodoServiceController } from "./todo-service.controller";
 import { TodoServiceService } from "./todo-service.service";
-import { ColorModule } from "./color/color.module";
-import { CourseModule } from "./course/course.module";
-import { CourseTagModule } from "./course-tag/course-tag.module";
-import { RepeatedDaysOfTheWeekModule } from "./repeated-days-of-the-week/repeated-days-of-the-week.module";
-import { TagModule } from "./tag/tag.module";
-import { WorkTodoModule } from "./work-todo/work-todo.module";
 
 @Module({
-  imports: [ColorModule, CourseModule, CourseTagModule, RepeatedDaysOfTheWeekModule, TagModule, WorkTodoModule],
+  imports: [],
   controllers: [TodoServiceController],
   providers: [TodoServiceService],
 })
